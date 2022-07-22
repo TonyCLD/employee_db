b843e5e77cc9143302573d27d9597e142e5f32ca
Anthony LoneFight <tonylonefight@gmail.com>
2022-07-21 19:50:38 -0500

    trying to do code from scratch

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const inquirer = require("inquirer");
-const axios = require("axios");
+// const inquirer = require("inquirer");
+// const axios = require("axios");
 
-
+const questions = [
+    {
+        name: 'name',
+        message: 'Enter name.'
+    },
+    {
+        name: 'id',
+        message: 'Enter employee ID.'
+    },
+    {
+        name: 'email',
+        message: 'Enter employee email.'
+    },
+];
