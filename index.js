817c69afbebee7a64b12b7cd200322391ca22593
Anthony LoneFight <tonylonefight@gmail.com>
2022-07-22 00:14:09 -0500

    getting the input working, abandoned previous work

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-// const inquirer = require("inquirer");
-// const axios = require("axios");
+const inquirer = require("inquirer");
+const axios = require("axios");
+const fs = require('fs');
+const { listenerCount } = require("process");
+// const { workerData } = require("worker_threads");
 
 const questions = [
     {
@@ -14,4 +17,26 @@ const questions = [
         name: 'email',
         message: 'Enter employee email.'
     },
+    {
+        type: 'list',
+        name: 'role',
+        message: 'Enter role of employee.',
+        choices: ['Manager', 'Engineer', 'Intern']
+    },
+    {
+        name: 'officeNum',
+        message: 'Enter the office of the manager.',
+    },
+    {
+        name: 'github_id',
+        message: 'Enter the github ID the engineer.',
+    },
+    {
+        name: 'school',
+        message: 'Enter the school of the intern.',
+    },
 ];
+
+inquirer.prompt(questions).then((data) => {
+        const datacombined1 = data;
+        console.log(datacombined1)});
