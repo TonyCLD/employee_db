6ef5020a67c33462d28f45331c163784029d7e8c
Anthony LoneFight <tonylonefight@gmail.com>
2022-07-22 10:07:59 -0500

    working on constructor function

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const inquirer = require("inquirer");
 const axios = require("axios");
 const fs = require('fs');
-const { listenerCount } = require("process");
-// const { workerData } = require("worker_threads");
+const Employee = require('./lib/Employee.js');
+const Engineer = require('./lib/Engineer.js');
 
 const questions = [
     {
@@ -10,7 +10,7 @@ const questions = [
         message: 'Enter name.'
     },
     {
-        name: 'id',
+        name: 'emp_id',
         message: 'Enter employee ID.'
     },
     {
@@ -39,4 +39,13 @@ const questions = [
 
 inquirer.prompt(questions).then((data) => {
         const datacombined1 = data;
-        console.log(datacombined1)});
+        console.log(datacombined1);
+    }.then(() => ) {const fullinfo2 = new Engineer (datacombined1.name, datacombined1.emp_id, datacombined1.email, datacombined1.role,);
+    console.log(fullinfo2);} );
+
+// const emp_card = new Employee(datacombined1);
+// console.log(emp_card);
+
+// const fullinfo = new Employee ('tony', 025845, 'tony@tony.com');
+// console.log(fullinfo);
+
